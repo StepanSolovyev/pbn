c15e3b3f3ee4c7a6990be863c8c353ef3e512eca
Stepan Solovyev <phobosvld@gmail.com>
2018-02-16 18:22:07 +0300

    init

---
+++ quotations/quotations/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(quotations.Startup))]
+namespace quotations
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
