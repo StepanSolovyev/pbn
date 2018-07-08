456f8501915abd13f3db861ead2d7e37a741fe85
Stepan Solovyev <phobosvld@gmail.com>
2018-07-08 20:12:37 +0300

    some soap updates

--- GetInflation/GetInflation/Program.cs
+++ GetInflation/GetInflation/Program.cs
@@ -9,11 +9,6 @@ using System.IO;
 using System.Web;
 using System.Net.Http;
 
-using System;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace GetInflation
 {
     class Program
@@ -22,43 +17,11 @@ namespace GetInflation
 
         static void Main(string[] args)
         {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(
-                "http://www.inflationinrussia.com/DesktopModules/WebServices.asmx/GetFirstMonth");
-            httpWebRequest.ContentType = "application/json; charset=utf-8";
-            httpWebRequest.Method = "POST";
-
-            // ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                string json = @"{}"; ;
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
-                Console.WriteLine(result);
-            }
+            Console.WriteLine(WebAPI.GetFirstMonth());
+            Console.ReadKey();
         }
 
     }
 }
 
 
-
-
-//string myJson = @"{startMonth: '' + '2010/01/01Z' + '\', endMonth : '' + '2010/05/01Z' + '\' }";
-//GetPageSizeAsync("http://www.inflationinrussia.com/DesktopModules/WebServices.asmx/GetInflation", myJson).Wait();
-//Console.Write("end\n");
-//        }
-//        private static async Task GetPageSizeAsync(string url, string json)
-//{
-//    using (var client = new HttpClient())
-//    {
-//        HttpResponseMessage response = await client.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json"));
-//        Console.WriteLine(response.Content.ReadAsStringAsync().Wait());
-//    }
---
+++ GetInflation/GetInflation/WebAPI.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Net.Http;
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Xml;
+namespace GetInflation
+{
+    enum SOAPAction : Int32 { GetFirstMonth, GetLastMonth, GetInflation, GetValueChange, GetPriceChange, BaseUri }
+    static class WebAPI
+    {
+
+
+        static string SOAPActionGetUri(SOAPAction action)
+        {
+            switch (action)
+            {
+                case SOAPAction.GetFirstMonth:
+                    return "http://www.inflationinrussia.com/WebServices/GetFirstMonth";
+                case SOAPAction.GetLastMonth:
+                    return "http://www.inflationinrussia.com/WebServices/GetLastMonth";
+                case SOAPAction.GetInflation:
+                    return "http://www.inflationinrussia.com/WebServices/GetInflation";
+                case SOAPAction.GetValueChange:
+                    return "http://www.inflationinrussia.com/WebServices/GetValueChange";
+                case SOAPAction.GetPriceChange:
+                    return "http://www.inflationinrussia.com/WebServices/GetPriceChange";
+
+                // base uri to perform request
+                case SOAPAction.BaseUri:
+                    return "http://inflationinrussia.com/DesktopModules/WebServices.asmx";
+
+                // debug purposes
+                default:
+                    return "error: SOAPActionGetUri returned default case.";
+                    ;
+
+            }
+
+
+        }
+        public static decimal GetPriceChange(Decimal startAmount, DateTime startMonth, DateTime endMonth, bool applyDenominationOf1998)
+        { return 0; }
+
+        public static decimal GetValueChange(Decimal startAmount, DateTime startMonth, DateTime endMonth, bool applyDenominationOf1998)
+        { return 0; }
+
+        public static decimal GetInflation(DateTime startMonth, DateTime endMonth)
+        { return 0; }
+
+        public static DateTime GetLastMonth() { return DateTime.Now; }
+        public static DateTime GetFirstMonth()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml("<soap:Envelope xmlns: xsi = 'http://www.w3.org/2001/XMLSchema-instance' xmlns: xsd = 'http://www.w3.org/2001/XMLSchema' xmlns: soap = 'http://schemas.xmlsoap.org/soap/envelope/'>" +
+                "<soap:Body>" +
+                "<GetFirstMonth xmlns = 'http://www.inflationinrussia.com/WebServices'/>" +
+                "</soap:Body>" +
+                "</soap:Envelope>");
+
+            xmldoc.CreateXmlDeclaration("1.0", "utf-8", null);
+
+            return DateTime.Now;
+        }
+
+
+        static string PerformWebRequest(XmlDocument xml, SOAPAction action)
+        {
+            WebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(SOAPActionGetUri(SOAPAction.BaseUri));
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.Headers.Add("SOAPAction", SOAPActionGetUri(action));
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                return streamReader.ReadToEnd();
+
+            }
+        }
+    }
+}
+
+/*
+вот павершел пример, его на C# потом простопереведем
+
+
+System.Xml xml = '<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <GetFirstMonth xmlns = "http://www.inflationinrussia.com/WebServices" />
+  </ soap:Body>
+</soap:Envelope>'
+
+$headers = @{"SOAPAction" = "http://www.inflationinrussia.com/WebServices/GetFirstMonth"}
+
+$URI = "http://inflationinrussia.com/DesktopModules/WebServices.asmx"
+[xml]$out = Invoke-WebRequest $uri -Method post -ContentType 'text/xml; charset=utf-8' -Body $SOAP -Headers $headers
+
+# show result 
+$out.DocumentElement.Body.GetFirstMonthResponse.GetFirstMonthResult
+*/
