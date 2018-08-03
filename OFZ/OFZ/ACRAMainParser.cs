d3d235c9a8db74e9c8333c1d97fd1c15d182f6c9
Irocool <ikuligina@gmail.com>
2018-08-03 21:14:07 +0300

    data from page 1

--- OFZ/OFZ/ACRAMainParser.cs
+++ OFZ/OFZ/ACRAMainParser.cs
@@ -1,47 +1,36 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 
-namespace OFZ
+namespace ACRAMain
 {
-    public struct maintable
-    {
-        public int emit;
-        public string name;//наименование
-        public string rating;//рейтинг
-        public string forecast; //прогноз/пересмотр
-        public string sector; //сектор
-        public string eregion; //регион
-        public string data; //дата
-        public ulong inn;
-        //public emitent(int emitid)
-        //{
-        //    emit = emitid;
-        //    name = "";
-        //    sector = "";
-        //    eregion = "";
-        //     inn = 0;
+    //public struct emitent
+    //{
+    //    public int emit;
+    //    public string name;//наименование
+    //    public string rating;//рейтинг
+   //     public string forecast; //прогноз/пересмотр
+    //    public string sector; //сектор
+   //     public string eregion; //регион
+   //     public DateTime data; //дата
+                                     //public emitent(int emitid)
+                                     //{
+                                     //    emit = emitid;
+                                     //    name = "";
+                                     //    sector = "";
+                                     //    eregion = "";
+                                     //     inn = 0;
 
         //}
     }
-    class ACRAMainParser
-    {
-        public maintable GetMainData(string url)
-        {
-            maintable MTable = new maintable();
-            string XP = "https://www.acra-ratings.ru/ratings/issuers/" + idemitent;
+    //public class ACRAMainParser
+    //{
 
-            HtmlAgilityPack.HtmlWeb webMT = new HtmlWeb();
-            webMT.OverrideEncoding = Encoding.GetEncoding("Windows-1251");
-            HtmlAgilityPack.HtmlDocument docMT = webMT.Load(XP);
-            Emit.emit = int.Parse(idemitent);
-            Emit.inn = ulong.Parse(doc1.DocumentNode.SelectSingleNode("/html/body/div/div[3]/div/section[2]/div/div/div[1]/div[3]/span[2]").InnerText.Replace(" ", ""));
-            return (Emit);
-        }
-        // HtmlAgilityPack.HtmlWeb webmain = new HtmlWeb();
+    //}    //HtmlAgilityPack.HtmlWeb webmain = new HtmlWeb();
 
         //public HtmlAgilityPack.HtmlDocument docmain = webmain.Load("https://www.acra-ratings.ru/ratings/issuers?order=press_release&page=1&sort=desc");
 
@@ -53,5 +42,77 @@ namespace OFZ
         //Console.WriteLine(featuredArticle.InnerHtml);
         //Console.ReadKey();
         //}
-    }
-}
+     // --------------------------  CreateTable----------------------------------
+     //   public static void CreateOrderTable()
+     //   {
+    //        // Create new DataTable.
+    //        DataTable orderTable = new DataTable();
+
+    //        // Declare DataColumn and DataRow variables.
+     //       DataColumn column;
+    //        DataRow row;
+
+            // Create new DataColumn, set DataType, ColumnName
+            // and add to DataTable.    
+    //        column = new DataColumn();
+    //        column.DataType = System.Type.GetType("System.String");
+    //        column.ColumnName = "name";
+    //        column.ReadOnly = false;
+    //        column.Unique = true;
+
+    //        orderTable.Columns.Add(column);
+
+            // Create second column.
+    //        column = new DataColumn();
+    //        column.DataType = Type.GetType("System.String");
+    //        column.ColumnName = "rating";
+    //        orderTable.Columns.Add(column);
+
+            // Create 3d column.
+    //        column = new DataColumn();
+    //        column.DataType = Type.GetType("System.String");
+    //        column.ColumnName = "forecast";
+     //       orderTable.Columns.Add(column);
+
+            // Create new DataRow objects and add to DataTable.    
+    //        for (int i = 0; i < 200; i++)
+    //        {
+    //            row = orderTable.NewRow();
+    //            row["name"] = i;
+    //            row["rating"] = "rating " + i;
+    //            row["forecast"] = "forecast " + i;
+    //            orderTable.Rows.Add(row);
+    //        }
+
+    //    }
+    //    public void InsertOrderDetails(DataTable orderTable)
+    //    {
+            // Use an Object array to insert all the rows .
+            // Values in the array are matched sequentially to the columns, based on the order in which they appear in the table.
+    //        Object[] rows = {
+    //                             new Object[]{1,"O0001","Mountain Bike",1419.5,36},
+    //                             new Object[]{2,"O0001","Road Bike",1233.6,16},
+    //                             new Object[]{3,"O0001","Touring Bike",1653.3,32},
+    //                             new Object[]{4,"O0002","Mountain Bike",1419.5,24},
+    //                             new Object[]{5,"O0002","Road Bike",1233.6,12},
+     //                            new Object[]{6,"O0003","Mountain Bike",1419.5,48},
+     //                            new Object[]{7,"O0003","Touring Bike",1653.3,8},
+     //                        };
+
+    //        foreach (Object[] row in rows)
+    //        {
+    //            orderTable.Rows.Add(row);
+     //       }
+//
+    //    }
+
+
+   // }
+   // DataTable orderTable = CreateOrderTable();
+    //InsertOrders(orderTable);
+    //ShowTable(orderTable);
+//}
+
+
+
+
---
+++ OFZ/OFZ/OFZ/ACRAMainParser.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace ACRAMain
+{
+    //public struct emitent
+    //{
+    //    public int emit;
+    //    public string name;//наименование
+    //    public string rating;//рейтинг
+   //     public string forecast; //прогноз/пересмотр
+    //    public string sector; //сектор
+   //     public string eregion; //регион
+   //     public DateTime data; //дата
+                                     //public emitent(int emitid)
+                                     //{
+                                     //    emit = emitid;
+                                     //    name = "";
+                                     //    sector = "";
+                                     //    eregion = "";
+                                     //     inn = 0;
+
+        //}
+    }
+    //public class ACRAMainParser
+    //{
+
+    //}    //HtmlAgilityPack.HtmlWeb webmain = new HtmlWeb();
+
+        //public HtmlAgilityPack.HtmlDocument docmain = webmain.Load("https://www.acra-ratings.ru/ratings/issuers?order=press_release&page=1&sort=desc");
+
+        //var featuredArticle = doc.DocumentNode.SelectSingleNode("/html/body/div[1]/section[2]/div/div/p/text()");
+        //*[@id="search-results"]/div/section/table/tbody/tr[1]/td[2]/span[2]
+        //   string text = featuredArticle.
+
+        //var featuredArticle = doc.DocumentNode.SelectSingleNode("/html/body/div/div[5]/div/table/tbody//tr");
+        //Console.WriteLine(featuredArticle.InnerHtml);
+        //Console.ReadKey();
+        //}
+     // --------------------------  CreateTable----------------------------------
+     //   public static void CreateOrderTable()
+     //   {
+    //        // Create new DataTable.
+    //        DataTable orderTable = new DataTable();
+
+    //        // Declare DataColumn and DataRow variables.
+     //       DataColumn column;
+    //        DataRow row;
+
+            // Create new DataColumn, set DataType, ColumnName
+            // and add to DataTable.    
+    //        column = new DataColumn();
+    //        column.DataType = System.Type.GetType("System.String");
+    //        column.ColumnName = "name";
+    //        column.ReadOnly = false;
+    //        column.Unique = true;
+
+    //        orderTable.Columns.Add(column);
+
+            // Create second column.
+    //        column = new DataColumn();
+    //        column.DataType = Type.GetType("System.String");
+    //        column.ColumnName = "rating";
+    //        orderTable.Columns.Add(column);
+
+            // Create 3d column.
+    //        column = new DataColumn();
+    //        column.DataType = Type.GetType("System.String");
+    //        column.ColumnName = "forecast";
+     //       orderTable.Columns.Add(column);
+
+            // Create new DataRow objects and add to DataTable.    
+    //        for (int i = 0; i < 200; i++)
+    //        {
+    //            row = orderTable.NewRow();
+    //            row["name"] = i;
+    //            row["rating"] = "rating " + i;
+    //            row["forecast"] = "forecast " + i;
+    //            orderTable.Rows.Add(row);
+    //        }
+
+    //    }
+    //    public void InsertOrderDetails(DataTable orderTable)
+    //    {
+            // Use an Object array to insert all the rows .
+            // Values in the array are matched sequentially to the columns, based on the order in which they appear in the table.
+    //        Object[] rows = {
+    //                             new Object[]{1,"O0001","Mountain Bike",1419.5,36},
+    //                             new Object[]{2,"O0001","Road Bike",1233.6,16},
+    //                             new Object[]{3,"O0001","Touring Bike",1653.3,32},
+    //                             new Object[]{4,"O0002","Mountain Bike",1419.5,24},
+    //                             new Object[]{5,"O0002","Road Bike",1233.6,12},
+     //                            new Object[]{6,"O0003","Mountain Bike",1419.5,48},
+     //                            new Object[]{7,"O0003","Touring Bike",1653.3,8},
+     //                        };
+
+    //        foreach (Object[] row in rows)
+    //        {
+    //            orderTable.Rows.Add(row);
+     //       }
+//
+    //    }
+
+
+   // }
+   // DataTable orderTable = CreateOrderTable();
+    //InsertOrders(orderTable);
+    //ShowTable(orderTable);
+//}
+
+
+
+
