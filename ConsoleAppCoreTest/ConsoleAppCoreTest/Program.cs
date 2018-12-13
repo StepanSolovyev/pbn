578f3ec5b31eedf6e3eb46d57941383357c4e46c
Stepan Solovyev <phobosvld@gmail.com>
2018-12-13 19:35:44 +0300

    test core project added

---
+++ ConsoleAppCoreTest/ConsoleAppCoreTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using RusBounds;
+
+namespace RusBounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            RBParser Parser = new RBParser();
+            
+            emitent[] Test = Parser.Start();
+
+            foreach (emitent item in Test)
+            {
+                Console.WriteLine(item.Issuer + " " + item.INN);
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+
